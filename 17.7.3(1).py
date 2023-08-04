ce3b8e94737979d8ad3a3845a50757def457039d
Вячеслав <Gremka90@Gmail.com>
2023-08-04 07:04:38 +0300

    Completed tasks

--- 17.7.3(1).py
+++ 17.7.3(1).py
@@ -4,7 +4,7 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
 data_item = per_cent.items()
 for key, value in data_item:
-    deposit.append(int(value * money))
+    deposit.append(value * money)
 
 print(deposit)
 print("Максимальная сумма вклада за год:", max(deposit))
--- 17.7.3(2).py
+++ 17.7.3(2).py
@@ -3,7 +3,7 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 
 deposit = per_cent.copy()
 for key, value in deposit.items():
-    deposit[key] = int(value * money)
+    deposit[key] = round(value/100, 3) * money
 
 print(deposit)
 print("Максимальная сумма вклада за год:", max(deposit.values()))
